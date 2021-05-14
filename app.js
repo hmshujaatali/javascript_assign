dce4f2e5d3e6d11a0595c59143781046309dcde7
SHUJAAT ALI <h.m.shujaatali786@gmail.com>
2021-05-14 23:03:59 +0500

    EDITED ALL QUESTIONS

---
+++ app.js
@@ -0,0 +1,28 @@
+                //Alert
+                //QUSETION#1
+//alert("WELCOME TO MY WEBSITE")
+                //QUSETION#2
+//alert("Hello, Mr Bean.\n How are you?")
+
+
+
+            //Variable for string
+                //QUSETION#1
+//var msg
+//msg="hello world
+//alert(msg)
+                //QUSETION#2
+//var book 
+//book="A smarter way to learn JavaScript"
+//document.write(book)
+
+
+
+            //Variable for Number
+//var age 
+//age=25
+//alert(age)
+
+//var birthYear 
+//birthYear=1995
+//document.write(birthYear)
